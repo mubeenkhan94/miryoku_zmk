de2ad79989862cb120d2ca9c7cb2394e820091f2
Mubeen Khan <8978476+mubeenkhan94@users.noreply.github.com>
2024-02-03 01:49:04 -0600

    revert removal of extra layer, make extra layer same as base layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+<<<<<<< Updated upstream
+=======
+#define MIRYOKU_ALPHAS=MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH
+#define MIRYOKU_EXTRA=MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH
+#define MIRYOKU_CLIPBOARD_MAC
+>>>>>>> Stashed changes
