8604ab70f86bbfff7566dd0ad2719d324a21df54
Mubeen Khan <8978476+mubeenkhan94@users.noreply.github.com>
2024-02-20 21:42:44 -0600

    make extended

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,22 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define U_MLK &kp LC(LG(Q))
+#define U_SCR &kp LS(LG(N2))
+#define U_OCR &kp LC(LA(LG(O)))
+#define U_CLP &kp LS(LG(V))
+
+#define MIRYOKU_LAYOUTMAPPING_PLANCK(\
+K00,   K01,      K02,      K03, K04,                   K05, K06, K07,    K08,       K09, \
+K10,   K11,      K12,      K13, K14,                   K15, K16, K17,    K18,       K19, \
+K20,   K21,      K22,      K23, K24,                   K25, K26, K27,    K28,       K29, \
+N30,   N31,      K32,      K33, K34,                   K35, K36, K37,    N38,       N39 \
+) \
+K00    K01       K02       K03  K04  &kp ESC  &kp RET  K05  K06  K07     K08        K09 \
+K10    K11       K12       K13  K14  U_NAV    &kp SQT  K15  K16  K17     K18        K19 \
+K20    K21       K22       K23  K24  U_SCR    U_OCR    K25  K26  K27     K28        K29 \
+U_MLK  &kp LEFT  &kp DOWN  K32  K33  K34      K35      K36  K37  &kp UP  &kp RIGHT  U_CLP
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(RSHFT, N),    U_MT(RCTRL, E),    U_MT(RALT, I),     U_MT(RGUI, O),     \
@@ -19,6 +35,12 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC), U_LT
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
 U_NP,              U_NP,              &kp ESC,           &kp BSPC,          &kp TAB,           &kp RET,           &kp SPACE,         &kp DEL,           U_NP,              U_NP
 
+#define MIRYOKU_LAYER_NUM \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
 #define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
@@ -31,6 +53,10 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp BSPC,          &kp TAB,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-#define MIRYOKU_MAPPING = EXTENDED_THUMBS
+#define MIRYOKU_LAYER_BUTTON \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_MLK,             U_SCR,             U_OCR,             U_CLP,             U_UND,             \
+U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
 #define MIRYOKU_CLIPBOARD = MAC
